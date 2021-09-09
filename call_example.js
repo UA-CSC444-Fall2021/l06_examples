35013534c672547903a8f2df896e1cd6f7b4e449
Joshua Levine <josh@email.arizona.edu>
2021-09-09 12:50:20 -0700

    Initial import

---
+++ call_example.js
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////
+//
+// Modifying l05.js to use .call()
+//
+// Author: Joshua Levine
+// Date: Sept. 9, 2021
+//
+////////////////////////////////////////////////////////////////
+
+
+// Data arrays
+let dataArray1 = [3,7,10,39,3,5,26,1,23,12];
+let dataArray2 = [2,3,5,7,11,13,17,19,23,29];
+let dataArray3 = [29,23,19,17];
+
+// Add the svg element
+let svg = d3.select("#div1").append("svg")
+
+// Initialize width/height
+svg.attr("width", 400)
+  .attr("height", 400)
+
+// Data Join on dataArray1 and setting attributes
+svg.selectAll("rect")
+  .data(dataArray1)
+  .enter()
+  .append("rect")
+  .call(setAttributes)
+
+
+function setAttributes(selection) {
+  selection
+    .attr("x", (x,i) => i*25)
+    .attr("y", function(d) { return 400 - d*10; })
+    .attr("width", 20)
+    .attr("height", function(d) { return d*10; } )
+    .attr("fill", d3.rgb(255,0,0));
+}
